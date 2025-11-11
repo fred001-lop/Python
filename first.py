3460c13f07996cfb597458c0eb1376acab1019eb
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-11-10 21:35:06 -0600

    modified first fiel

--- first.py
+++ first.py
@@ -1,4 +1,40 @@
-print('La witsi araña\nsubio la telaraña')
-print()
-print("Vino la lluvia y se la llevo")
-print('Witsi witsi', 'Araña', 'Es de caramelo')
+# print('La witsi araña\nsubio la telaraña')
+# print()
+# print("Vino la lluvia y se la llevo")
+# print('Witsi witsi', 'Araña', 'Es de caramelo')
+
+# print("Hola mi nombre es ", end="\n")
+# print("This is a message")
+
+# print('My', 'name', "is", 'Freddy', sep="-")
+
+# print("My", "Nombre", "es", sep="_", end="*")
+# print("Monty", "Python", sep="*", end="*\n")
+
+
+# Flechha
+print("    *\n"
+      "   * *\n"
+      "  *   *\n"
+      "***   ***\n"
+      "  *   *\n"
+      "  *   *\n"
+      "  *****"
+)
+
+print("        *        "*2)
+print("       * *       "*2)
+print("      *   *      "*2)
+print("     *     *     "*2)
+print("    *       *    "*2)
+print("   *         *   "*2)
+print("  *           *  "*2)
+print(" *             * "*2)
+print("******     ******"*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *******     "*2)
