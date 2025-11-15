74bcf5707aaacfc866148952f9e951a0bfe39d85
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-11-14 23:28:13 -0600

    add new files

---
+++ variables.py
@@ -0,0 +1,22 @@
+# var = 10
+# print(var)
+
+# var= 1
+# account_balance = 1000.0
+# client_name = 'John Dou'
+
+# print(var, account_balance, client_name)
+# print(var)
+
+# var = '3.8.6'
+# print("Python version " + var)
+
+# var = 1
+# print(var)
+
+# var = var + 1
+# print(var)
+
+var = 100
+var = 200 + 300
+print(var)
