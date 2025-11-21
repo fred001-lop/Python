1d6fac8d1c2754cf5f87572d5e56d9ba2f636e28
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-11-20 23:14:42 -0600

    new files & modified old

---
+++ inputs.py
@@ -0,0 +1,10 @@
+# print("Dime lo que deseas")
+# anything = input()
+# print("Hmm", anything , "...En, serio")
+
+# anything = input("Dime lo que deseas")
+# print("hmm", anything, "en serio?")
+
+anything = input("Ingresa un numero")
+something = anything ** 2.0
+print("Este es el resultado", something)
