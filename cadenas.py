a01cd346214aacb8faf9a20fa14835bb4f3e1778
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-12-07 00:23:33 -0600

    new file & modified file variables

---
+++ cadenas.py
@@ -0,0 +1,4 @@
+fnam = input("Me puedes dar tu nombre porfavor: ")
+fapell = input("Me puedes dar tu apellido: ")
+print("gracias")
+print("\n Tu informaicon es: ", fnam + "" + fapell)
--- variables.py
+++ variables.py
@@ -40,5 +40,5 @@
 
 leg_a = float(input("Ingrese la longitud del primer cateto"))
 leg_b = float(input("Ingrese la longitud del segundo cateto"))
-hypo = (leg_a ** 2 + leg_b ** 2) ** .5
-print("La longitud de la hipotenusa es:", hypo)
+# hypo = (leg_a ** 2 + leg_b ** 2) ** .5
+print("La longitud de la hipotenusa es:", (leg_a ** 2 + leg_b ** 2) ** .5)
