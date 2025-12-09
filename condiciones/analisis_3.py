f455876dab6930faf6f157b1bf3bbeab10c8c100
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-12-08 18:31:09 -0600

    Se creo un archivo que simplifica los if y otro archivo para un lab

---
+++ condiciones/analisis_3.py
@@ -0,0 +1,9 @@
+
+number_1 = int(input("Ingrese el primer valor: "))
+number_2 = int(input("Ingrese el primer valor: "))
+number_3 = int(input("Ingrese el primer valor: "))
+
+
+mayorNumber = min(number_1, number_2, number_3)
+
+print("El numero mayor es:", mayorNumber)
